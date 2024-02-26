c87ae0d4a205f18275647cc3256b5a0a8619f33f
flixoflax <flixoflax@gmx.de>
2024-02-26 20:58:21 +0100

    Add sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,38 @@
+import { loadEnv } from "vite";
+const {
+  PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  PUBLIC_SANITY_STUDIO_DATASET,
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
+const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
+
+import { sanityIntegration } from "@sanity/astro";
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    webAnalytics: {
+      enabled: true,
+    },
   }),
+  integrations: [
+    tailwind(),
+    sanityIntegration({
+      projectId,
+      dataset,
+      studioBasePath: "/admin",
+      useCdn: false,
+      // `false` if you want to ensure fresh data
+    }),
+    react(),
+  ],
 });
---
+++ schema/index.ts
@@ -0,0 +1,4 @@
+import blockContent from "./blockContent";
+import caseStudy from "./caseStudy";
+
+export const schemaTypes = [blockContent, caseStudy];
