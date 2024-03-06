a3ed48cad696dd5fe4148bc9b24a89995dad9611
flixoflax <flixoflax@gmx.de>
2024-03-06 16:44:23 +0100

    Add canoncial, description, client load times

--- src/components/Youtube.tsx
+++ src/components/Youtube.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import YouTubePlayer from "react-player/youtube";
 
 import type { TypedObject } from "astro-portabletext/types";
+import YouTubePlayer from "react-player/youtube";
 
 export interface Props extends TypedObject {
   url: string;
@@ -14,7 +14,17 @@ export function isYoutubeEmbed(t: unknown): t is Props {
 export function Youtube(props: Props) {
   return (
     <div className="aspect-video">
-      <YouTubePlayer width={"100%"} height={"100%"} url={props.url} />
+      <YouTubePlayer
+        config={{
+          playerVars: {
+            cc_load_policy: 1,
+            cc_lang_pref: "en",
+          },
+        }}
+        width={"100%"}
+        height={"100%"}
+        url={props.url as string}
+      />
     </div>
   );
 }
